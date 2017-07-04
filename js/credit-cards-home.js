6558873d33a7d58bdc57683d281395604f0eb3ab
Martin Ferrari <dmferrari@gmail.com>
2017-07-04 16:06:18 -0300

    [#147868287] Dropdown fixes

--- js/credit-cards-home.js
+++ js/credit-cards-home.js
@@ -1,30 +1,41 @@
 $(document).ready(function () {
-  $("#btn-dropdown-score").click(function (e) {
-    e.preventDefault();
-    document.getElementById("custom-dropdown-score").classList.toggle("show");
+  $("#btn-dropdown-score").click(function () {
+    $("#custom-dropdown-score").toggleClass("show");
+    $("#custom-dropdown-action").removeClass("show");
   });
 
-  $("#btn-dropdown-action").click(function (e) {
-    e.preventDefault();
-    document.getElementById("custom-dropdown-action").classList.toggle("show");
+  $("#btn-dropdown-action").click(function () {
+    $("#custom-dropdown-action").toggleClass("show");
+    $("#custom-dropdown-score").removeClass("show");
   });
 
-  $('#custom-dropdown-score a').click(function (e) {
-    e.preventDefault();
-    $('#btn-dropdown-score').text($(this).text());
-    $("#btn-dropdown-action").trigger("click");
+  $("#custom-dropdown-score a").click(function () {
+    $("#btn-dropdown-score").text($(this).text());
   });
 
-  $('#custom-dropdown-action a').click(function (e) {
-    e.preventDefault();
-    $('#btn-dropdown-action').text($(this).text());
+  $("#custom-dropdown-action a").click(function () {
+    $("#btn-dropdown-action").text($(this).text());
   });
 
-  $('#cardsForm').validate({
-    messages: {
-      score: "Choose Your Credit Card Score",
-      category: "Choose What You Would Like to Do"
+  $("a[class*='link-']").click(function(e) {
+    e.preventDefault();
+  });
+
+  window.onclick = function (event) {
+    if (!event.target.matches('.custom-dropbtn')) {
+      var dropdowns = document.getElementsByClassName("custom-dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
     }
-  });
-  document.getElementById('btn-get-your-free-credit-score').setAttribute('href', URL_GET_FREE_SCORE);
+    if (event.target.matches('#custom-dropdown-score a')) {
+      $("#custom-dropdown-action").toggleClass("show");
+    }
+  }
+
+  document.getElementById("btn-get-your-free-credit-score").setAttribute("href", URL_GET_FREE_SCORE);
 });
