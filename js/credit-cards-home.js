6d84d5793385f449bcdb6d2e90001493eecc8311
Martin Ferrari <dmferrari@gmail.com>
2017-07-04 13:45:16 -0300

    [#147868287] Added custom dropdowns

--- js/credit-cards-home.js
+++ js/credit-cards-home.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#btn-dropdown-score").click(function (e) {
+    e.preventDefault();
+    document.getElementById("custom-dropdown-score").classList.toggle("show");
+  });
+
+  $("#btn-dropdown-action").click(function (e) {
+    e.preventDefault();
+    document.getElementById("custom-dropdown-action").classList.toggle("show");
+  });
+
+  $('#custom-dropdown-score a').click(function (e) {
+    e.preventDefault();
+    $('#btn-dropdown-score').text($(this).text());
+    $("#btn-dropdown-action").trigger("click");
+  });
+
+  $('#custom-dropdown-action a').click(function (e) {
+    e.preventDefault();
+    $('#btn-dropdown-action').text($(this).text());
+  });
+
   $('#cardsForm').validate({
     messages: {
       score: "Choose Your Credit Card Score",
---
+++ js/custom-dropdown.js
@@ -0,0 +1,16 @@
+
+
+
+
+window.onclick = function (event) {
+  if (!event.target.matches('.custom-dropbtn')) {
+    var dropdowns = document.getElementsByClassName("custom-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
